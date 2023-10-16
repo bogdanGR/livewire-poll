068bb5d07f6921fbad48f8c3e772df55149499c2
bogdanGR <fant1k450@gmail.com>
2023-10-16 08:33:40 +0300

    lirewire actions

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,8 +7,23 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = [''];
     public function render()
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    /**
+     * Initialize properties of the componenet
+     * @return void
+     */
+//    public function mount()
+//    {
+//
+//    }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -5,5 +5,18 @@
         <input wire:model.live="title" type="text"  />
 
         Current title: {{ $title }}
+
+        <div class="mt-4 mb-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
+        <div>
+            @foreach($options as $index => $option)
+
+                <div class="mb-4 mb-4">
+                    {{ $index }} - {{ $option }}
+                </div>
+            @endforeach
+        </div>
+
     </form>
 </div>
